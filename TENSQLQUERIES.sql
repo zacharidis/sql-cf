a86e29f9a4d738510fdbb940bfcf4cfd480b70d1
zacharidis <zacharidis@icloud.com>
2022-05-24 10:46:07 +0300

    queries 5 ,6 and 7 added

--- TENSQLQUERIES.sql
+++ TENSQLQUERIES.sql
@@ -30,3 +30,37 @@ order by model_year asc
    SELECT distinct production.products.product_name as "Onoma Proiontos" , production.products.list_price as "timi"
 from production.products ,sales.orders  
 order by list_price desc
+
+/* erotima 5o 
+na emfanistoun taproionta pou paragei i eteria electra */
+SELECT  production.products.product_name , production.brands.brand_name
+from production.products , production.brands
+where  production.brands.brand_name ='Electra'
+
+/* ερωτημα 6ο να εμφανιστουν τα προιοντα που κοστιζουν απο 100 εως 1000 δολαρια */
+
+SELECT distinct production.products.product_name , production.products.list_price
+from production.products ,sales.orders  
+where production.products.list_price between 100 and 1000 
+order by list_price asc
+
+
+/************************************ ερωτηματα για τις πωλησεις *****************************/
+
+/* ερωτημα 7ο εμφανισε τους πελατες που εχου βαλει παραγγελια μεγαλυτερη απο 1000 ευρω */
+
+SELECT DISTINCT SALES.customers.customer_id , 
+SALES.customers.first_name AS NAME , 
+SALES.customers.last_name AS SURNAME,
+order_items.list_price as PRICE
+
+FROM 
+
+sales.customers ,
+sales.order_items,
+sales.orders
+
+WHERE 
+
+sales.customers.customer_id = sales.orders.customer_id
+and sales.order_items.list_price > 1000
