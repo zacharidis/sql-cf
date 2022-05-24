f3f5dd7fb28cacff11f7491303eff56c7f5680a1
zacharidis <zacharidis@icloud.com>
2022-05-24 10:38:38 +0300

    queries 2 and 3 added

--- TENSQLQUERIES.sql
+++ TENSQLQUERIES.sql
@@ -6,4 +6,19 @@
    SELECT DISTINCT(BRAND_NAME) AS "MANUFACTURER NAME" FROM PRODUCTION.brands 
    ORDER BY BRAND_NAME ASC 
 
+
+   /*EROTIMA 2
+    OLA TA APOTHEMATA MAS ANA PROION KAI ANA KATASTIMA APO TO MEGALYTERO STO MIKROTERO
+     */
+
+	SELECT DISTINCT(production.stocks.store_id) ,(production.stocks.product_id) ,production.stocks.quantity  FROM production.stocks
+	ORDER BY PRODUCTION.stocks.quantity DESC
+
+
+	/* ΕΡΩΤΗΜΑ 3ο 
+να εμφανιστουν τα προιοντα μας απο το παλαιοτερο στο νεοτερο  */ 
+
+SELECT distinct production.products.product_name , production.products.model_year
+from production.products ,sales.orders  
+order by model_year asc
    
