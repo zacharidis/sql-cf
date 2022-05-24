14ae868d8d95070ab1e479ef333bcdf3ff4705e2
zacharidis <zacharidis@icloud.com>
2022-05-24 10:05:03 +0300

    added first query

---
+++ TENSQLQUERIES.sql
@@ -0,0 +1,9 @@
+﻿/** 10 ερωτηματα για εξαγωγη χρησιμων πληροφοριων απο την βαση bikerstore */
+
+/* ΕΡΩΤΗΜΑ 1 
+   ολα τα διαθεσιμα αποθεματα μας */
+   
+   SELECT DISTINCT(BRAND_NAME) AS "MANUFACTURER NAME" FROM PRODUCTION.brands 
+   ORDER BY BRAND_NAME ASC 
+
+   
